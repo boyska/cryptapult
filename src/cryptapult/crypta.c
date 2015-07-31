1361fa853e35162f65a49a8a6c37d50a31dfa022
boyska <piuttosto@logorroici.org>
2015-07-31 14:03:49 +0200

    crypto_box -> crypto_box_seal

--- src/cryptapult/crypta.c
+++ src/cryptapult/crypta.c
@@ -8,13 +8,6 @@
 #include "cryptutils.h"
 #include <sodium.h>
 
-unsigned char sk[32] = {
- 0x77,0x07,0x6d,0x0a,0x73,0x18,0xa5,0x7d
-,0x3c,0x16,0xc1,0x72,0x51,0xb2,0x66,0x45
-,0xdf,0x4c,0x2f,0x87,0xeb,0xc0,0x99,0x2a
-,0xb1,0x77,0xfb,0xa5,0x1d,0xb9,0x2c,0x2a
-} ;
-
 unsigned char pk[32] = {
  0xde,0x9e,0xdb,0x7d,0x7b,0x7d,0xc1,0xb4
 ,0xd3,0x5b,0x61,0xc2,0xec,0xe4,0x35,0x37
@@ -22,11 +15,6 @@ unsigned char pk[32] = {
 ,0xad,0xfc,0x7e,0x14,0x6f,0x88,0x2b,0x4f
 } ;
 
-unsigned char nonce[24] = {
- 0x69,0x69,0x6e,0xe9,0x55,0xb6,0x2b,0x73
-,0xcd,0x62,0xbd,0xa8,0x75,0xfc,0x73,0xd6
-,0x82,0x19,0xe0,0x03,0x6b,0x7a,0x0b,0x37
-} ;
 int fileno(FILE*);
 
 
@@ -144,17 +132,8 @@ int main(int argc, char **argv)
 	    return 1;
     }
 
-    c = calloc(plain_len + crypto_box_ZEROBYTES + crypto_box_BOXZEROBYTES,
-		    sizeof(char));
-    sodium_memzero(c, plain_len + crypto_box_ZEROBYTES +
-		    crypto_box_BOXZEROBYTES);
-    unsigned char precomputation[crypto_box_BEFORENMBYTES];
-    time_t starttime = time(NULL);
-    crypto_box_beforenm(precomputation, pk, sk);
-    if(params.benchmark) {
-	    fprintf(stderr, "Time of precomputation: %ld\n",
-			    (time(NULL) - starttime));
-    }
+    c = calloc(plain_len + crypto_box_SEALBYTES, sizeof(char));
+    sodium_memzero(c, plain_len + crypto_box_SEALBYTES);
     if(!params.benchmark) {
 	    if(params.check_tty && isatty(fileno(stdout))) {
 		    fprintf(stderr, "Output is a tty, refusing to write\n");
@@ -162,16 +141,16 @@ int main(int argc, char **argv)
 		    sodium_free(plain);
 		    return 10;
 	    }
-	    const int r = encrypt(c, precomputation, nonce, plain, plain_len);
-	    if(r < 0) {
+	    const int r = crypto_box_seal(c, plain, plain_len, pk);
+	    if(r != 0) {
 		    fprintf(stderr, "Error %d occured\n", r);
 		    return 1;
 	    }
-	    fwrite(c, sizeof(unsigned char), r, stdout);
+	    fwrite(c, sizeof(unsigned char), plain_len + crypto_box_SEALBYTES , stdout);
     } else {
-	    starttime = time(NULL);
+	    time_t starttime = time(NULL);
 	    for(int i=0; i < params.benchmark_count; i++) {
-		    encrypt(c, precomputation, nonce, plain, plain_len);
+		    crypto_box_seal(c, plain, plain_len, pk);
 	    }
 	    fprintf(stderr, "Time per cycle: %.3f\n",
 			    (double)(time(NULL) - starttime) / params.benchmark_count);
