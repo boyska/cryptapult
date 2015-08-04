54c1914c8ac7ec5b77083d2df975ac8c306837fa
dbz <foo@bar.com>
2015-08-04 15:48:24 +0200

    fileutils: some i/o error checking

--- src/cryptapult/fileutils.c
+++ src/cryptapult/fileutils.c
@@ -11,26 +11,37 @@ long file_readwhole(char *filename, unsigned char **buf)
     long fsize;
     FILE *f = fopen(filename, "rb");
     if (f == NULL) {
-	fprintf(stderr, "Failure reading '%s' (non existent file?)\n",
-		filename);
-	return -1;
+        fprintf(stderr, "Failure reading '%s' (non existent file?)\n",
+                filename);
+        return -1;
     }
-    fseek(f, 0, SEEK_END);
+    if (fseek(f, 0, SEEK_END) != 0) {
+        fprintf(stderr, "Failure reading '%s'\n", filename);
+        return -1;
+    }
     fsize = ftell(f);
-    fseek(f, 0, SEEK_SET);
+    if (fsize == -1) {
+        fprintf(stderr, "Failure reading '%s'\n", filename);
+        return -1;
+    }
+    if (fseek(f, 0, SEEK_SET)) {
+        fprintf(stderr, "Failure reading '%s'\n", filename);
+        return -1;
+    }
 
     content = sodium_allocarray(fsize, sizeof(char));
     if (content == NULL) {
-	fprintf(stderr, "Failure reading '%s' (not enough memory?)\n",
-		filename);
-	return -2;
+        fprintf(stderr, "Failure reading '%s' (not enough memory?)\n",
+                filename);
+        return -2;
     }
     size_t rbytes = fread(content, sizeof(char), fsize, f);
     if (rbytes != (size_t) fsize) {
-	fprintf(stderr,
-		"Failure reading '%s' (%zu bytes read instead of %ld)\n",
-		filename, rbytes, fsize);
-	return -3;
+        fprintf(stderr,
+                "Failure reading '%s' (%zu bytes read instead of %ld)\n",
+                filename, rbytes, fsize);
+        sodium_free(content);
+        return -3;
     }
     fclose(f);
 
@@ -38,3 +49,5 @@ long file_readwhole(char *filename, unsigned char **buf)
     *buf = content;
     return fsize;
 }
+
+/* vim: set et ts=4 sw=4 */
